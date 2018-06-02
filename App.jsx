24ddc7c53fdd3c890c117f5f772a5aae4b9b3ddc
Kristina Stefanova <kris.stefanova@gmail.com>
2018-06-02 19:37:32 +1000

    Init React

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello Book List!!! Where have you been all my life?
+         </div>
+      );
+   }
+}
+export default App;
