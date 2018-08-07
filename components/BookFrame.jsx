256430da8ecc92bc653abbc035f2ef8f0336fdcf
Kristina Stefanova <kris.stefanova@gmail.com>
2018-08-07 20:59:09 +1000

    add background

--- components/BookFrame.jsx
+++ components/BookFrame.jsx
@@ -2,15 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 import styles from './BookFrame.scss'
 
+const BooksBackground = styled.div`
+  display:flex;
+  align-items: flex-start;
+  justify-content: center;
+  width: 987px;
+  height: 746px;
+  background-image: url("../styles/images/bookshelve.jpg")
+`
+
 const BookWrapper = styled.div`
   display: flex;
   align-items: center;
   background: #212121;
   justify-content: center;
-  width: 50%;
+  width: 65%;
   border: 1px solid black;
   font: "RobotoDraft", sans-serif;
   color: #fff;
+  margin-top: 12px;
 `
 
 const BookDetails = styled.div`
@@ -58,26 +68,28 @@ class BookFrame extends React.Component {
   render() {
     
     return (
-      <BookWrapper>
-        <ReadCheck>
-          <div>
-            <input id='one' type='checkbox' />
-            <label for='one'>
-              <span></span>
-            </label>
-          </div>
-        </ReadCheck>
-        
-        <BookDetails>
-          <h1>1. Harry Potter Series</h1>
-          <h2>by J.K. Rowling</h2>
-          <p>Good Reads rating: 4.62</p>
-          <p>Good Reads number of ratings 1,987,629</p>
-        </BookDetails>
-        <BookImg> 
-          <img src='https://images.gr-assets.com/books/1474171184s/136251.jpg' />
-        </BookImg>
-      </BookWrapper>
+      <BooksBackground>
+        <BookWrapper>
+          <ReadCheck>
+            <div>
+              <input id='one' type='checkbox' />
+              <label for='one'>
+                <span></span>
+              </label>
+            </div>
+          </ReadCheck>
+          
+          <BookDetails>
+            <h1>1. Harry Potter Series</h1>
+            <h2>by J.K. Rowling</h2>
+            <p>Good Reads rating: 4.62</p>
+            <p>Good Reads number of ratings 1,987,629</p>
+          </BookDetails>
+          <BookImg> 
+            <img src='https://images.gr-assets.com/books/1474171184s/136251.jpg' />
+          </BookImg>
+        </BookWrapper>
+      </BooksBackground>
     )
   }
 }
